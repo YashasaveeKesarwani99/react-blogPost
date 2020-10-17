8170d9936bf5c2dd8601bd490c0f54007df1f329
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-10-17 21:53:12 +0530

    fetching data to display full post

--- src/components/Fullpost/FullPost.js
+++ src/components/Fullpost/FullPost.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import "./FullPost.css";
 
 class FullPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      postFull: {}
+    };
+  }
+  //for updating state
+  componentDidUpdate() {
+    if (this.props.ID) {
+      if (!this.state.postFull || this.state.postFull.id !== this.props.ID)
+        axios
+          .get(`https://jsonplaceholder.typicode.com/posts/${this.props.ID}`)
+          .then((res) => {
+            //console.log(res);
+            this.setState({ postFull: res.data });
+            console.log(this.state.postFull);
+          });
+    }
+  }
+
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (this.props.ID) {
+      post = <p style={{ textAlign: "center" }}>Loading...!</p>;
+    }
+    if (this.state.postFull.id) {
       post = (
         <div className="FullPost">
-          <h1>Title</h1>
-          <p>Content</p>
+          <h1>{this.state.postFull.title}</h1>
+          <p>{this.state.postFull.body}</p>
           <div className="Edit">
             <button className="Delete">Delete</button>
           </div>
