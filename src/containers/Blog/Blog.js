8ac36f5efb736267b0e12ecbdb1306ed442edf74
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-10-21 16:29:10 +0530

    Routing added for home page and newpost problem - page is reloading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
 import "./Blog.css";
-
+import { Route } from "react-router-dom";
 import Posts from "../Blog/Posts/Posts";
-
+import NewPost from "../Blog/Newpost/NewPost";
 class Blog extends Component {
   render() {
     return (
@@ -15,12 +15,13 @@ class Blog extends Component {
                 <a href="/">Home</a>
               </li>
               <li>
-                <a href="/">New Post</a>
+                <a href="/new-post">New Post</a>
               </li>
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
